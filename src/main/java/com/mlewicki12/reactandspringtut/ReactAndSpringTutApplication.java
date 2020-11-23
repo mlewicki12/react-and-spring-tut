8858811c8d6b8595a71fbb019e939df8ee87bb73
Maciej Lewicki <lewickimapl@gmail.com>
2020-11-23 16:31:28 +0100

    add initial project

---
+++ src/main/java/com/mlewicki12/reactandspringtut/ReactAndSpringTutApplication.java
@@ -0,0 +1,13 @@
+package com.mlewicki12.reactandspringtut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringTutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlewicki12/reactandspringtut/ReactAndSpringTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlewicki12.reactandspringtut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
